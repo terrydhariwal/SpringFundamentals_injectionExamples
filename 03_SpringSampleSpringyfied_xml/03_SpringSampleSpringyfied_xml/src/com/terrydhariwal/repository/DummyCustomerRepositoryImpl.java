4ae157751494364cd7e7a3672858ad1090c19ca0
Terry Dhariwal <terry.dhariwal@me.com>
2015-01-08 12:01:47 +0000

    added auto-wiring examples -> by constructor, byType and byName

---
+++ 03_SpringSampleSpringyfied_xml/03_SpringSampleSpringyfied_xml/src/com/terrydhariwal/repository/DummyCustomerRepositoryImpl.java
@@ -0,0 +1,14 @@
+package com.terrydhariwal.repository;
+
+import com.terrydhariwal.model.Customer;
+
+import java.util.List;
+
+public class DummyCustomerRepositoryImpl implements CustomerRepository {
+
+    @Override
+    public List<Customer> findAll() {
+        return null;
+    }
+
+}
---
+++ 03_SpringSampleSpringyfied_xml/03_SpringSampleSpringyfied_xml/src/com/terrydhariwal/service/DummyCustomerServiceImpl.java
@@ -0,0 +1,12 @@
+package com.terrydhariwal.service;
+
+import com.terrydhariwal.model.Customer;
+
+import java.util.List;
+
+public class DummyCustomerServiceImpl implements CustomerService {
+    @Override
+    public List<Customer> findAll() {
+        return null;
+    }
+}
